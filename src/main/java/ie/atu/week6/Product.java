669e4c2c3e72567e6e921ab25b80c9532600e6b6
AndrewFoxATU <G00417373@atu.ie>
2024-10-23 15:48:13 +0100

    added get and put and delete

--- src/main/java/ie/atu/week6/Product.java
+++ src/main/java/ie/atu/week6/Product.java
@@ -1,5 +1,7 @@
 package ie.atu.week6;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class Product {
+    @Min(value=1, message=" ID must be greater or equal to 1")
     private int id;
+    @NotBlank(message = "You must enter a name")
     private String name;
+    @Min(value=1, message="price must be greater then 0")
     private double price;
 
 
